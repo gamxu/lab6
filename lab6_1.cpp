e337fa81a527666cca9f72253b78cee6c2a4aa10
gamxu <wuttipat.did@gmail.com>
2021-11-26 16:11:51 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,36 @@
 using namespace std;
 
 int main(){
+
+    int N, countEven = 0, countOdd = 0;
+
     cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    cin >> N;
+
+    if(N % 2 == 0){
+            countEven++;
+
+        }else{
+            countOdd++;
+        }
+        
+    while(N != 0){
+
+    
+    cout << "Enter an integer: ";
+    cin >> N;
+    
+        if(N % 2 == 0){
+            countEven++;
+
+        }else{
+            countOdd++;
+        }
+
+    }
+  
+    cout << "#Even numbers = " << countEven-1;
+    cout << "#Odd numbers = " << countOdd;
     return 0;
 }
+
